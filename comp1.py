563ec705d66bbb197bd9fb895656a22418b78268
root <jerry110160@gmail.com>
2017-12-26 17:18:04 -0500

    handle the presence of config files

--- comp1.py
+++ comp1.py
@@ -1,8 +1,46 @@
 from datetime import datetime
 import sys
 import argparse
+import os
+import shutil
 
-# add positional arguments
+
+def locallog(message,historyFile='history.txt'):
+    history = open(historyFile,"a+")
+    history.write(message)
+    history.write("\n")
+
+def compareConfigFiles(deployedFile,archivedFile):
+
+    with open(deployedFile) as f:
+        t1 = f.read().splitlines()
+        t1s = set(t1)
+
+
+
+    with open(archivedFile) as f:
+        t2 = f.read().splitlines()
+        t2s = set(t2)
+
+    #-- in file1 but not file2
+    for diff in t1s-t2s:
+        log1="%s Only in deployed file %s %s \n"%(str(datetime.now()),t1.index(diff), diff)
+        print (log1)
+        locallog(log1)
+        #history.write(log1)
+        #history.write("\r\n")
+        #print (str(datetime.now()),"Only in file1",t1.index(diff), diff)
+
+    #-- in file2 but not file1
+    for diff in t2s-t1s:
+        log2="%s Only in archived file %s %s \n"%(str(datetime.now()),t2.index(diff), diff)
+        print (log2)
+        locallog(log2)
+        #history.write(log2)
+        #history.write("\r\n")
+
+
+#-- add positional arguments
 
 parser = argparse.ArgumentParser(description="Log changes of configuration fles ")
 parser.add_argument("deployedFile",help='complete path of a deployed file')
@@ -16,36 +54,25 @@ print ("Second Argument",args.archivedFile)
 
 
 
-#file1.write("test")
+if not os.path.isfile(args.deployedFile):
+    print("stop compare and add logs")
+    msg="%s Deplyed file not exists \n"%(str(datetime.now()))
+    locallog(msg)
+else:
+    print("deployed file exists")
+    if os.path.isfile(args.archivedFile):
+        print("archived file exists")
+        compareConfigFiles(args.deployedFile,args.archivedFile)
+    else:
+        print("archived file not exits")
+        shutil.copy2(args.deployedFile,args.archivedFile)
+        msg="%s Archived file not exists; Copied deployed file \n"%(str(datetime.now()))
+        locallog(msg)
 
-#file1.close()
-#-- open log file
-history = open("history.txt","a+")
 
-#with open('./local.properties') as f:
-with open(args.deployedFile) as f:
-    t1 = f.read().splitlines()
-    t1s = set(t1)
 
-#with open('./local_a.properties') as f:
-with open(args.archivedFile) as f:
-    t2 = f.read().splitlines()
-    t2s = set(t2)
 
-#-- in file1 but not file2
-for diff in t1s-t2s:
-    log1="%s Only in deployed file %s %s \n"%(str(datetime.now()),t1.index(diff), diff)
-    print (log1)
-    history.write(log1)
-    history.write("\r\n")
-    #print (str(datetime.now()),"Only in file1",t1.index(diff), diff)
 
-#-- in file2 but not file1
-for diff in t2s-t1s:
-    log2="%s Only in archived file %s %s \n"%(str(datetime.now()),t2.index(diff), diff)
-    print (log2)
-    history.write(log2)
-    history.write("\r\n")
-    #print (str(datetime.now()),"Only in file2",t2.index(diff), diff)
+
 
 #send out notification when discrepancy exists
