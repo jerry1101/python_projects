a0c131444f1729f22485b8b73dbf35562cc96a0a
jerry <jerry110160@gmail.com>
2018-01-09 14:22:45 -0500

    refactoring comp1.py

--- comp1.py
+++ comp1.py
@@ -5,20 +5,19 @@ import os
 import shutil
 
 
-def locallog(message,historyFile='history.txt'):
-    history = open(historyFile,"a+")
+def locallog(message, history_file='history.txt'):
+    history = open(history_file, "a+")
     history.write(message)
     history.write("\n")
 
-def compareConfigFiles(deployedFile,archivedFile):
 
-    with open(deployedFile) as f:
+def compareConfigFiles(deployed_file, archive_file):
+
+    with open(deployed_file) as f:
         t1 = f.read().splitlines()
         t1s = set(t1)
 
-
-
-    with open(archivedFile) as f:
+    with open(archive_file) as f:
         t2 = f.read().splitlines()
         t2s = set(t2)
 
@@ -27,36 +26,29 @@ def compareConfigFiles(deployedFile,archivedFile):
         log1="%s Only in deployed file %s %s \n"%(str(datetime.now()),t1.index(diff), diff)
         print (log1)
         locallog(log1)
-        #history.write(log1)
-        #history.write("\r\n")
-        #print (str(datetime.now()),"Only in file1",t1.index(diff), diff)
 
     #-- in file2 but not file1
     for diff in t2s-t1s:
         log2="%s Only in archived file %s %s \n"%(str(datetime.now()),t2.index(diff), diff)
         print (log2)
         locallog(log2)
-        #history.write(log2)
-        #history.write("\r\n")
 
 
 #-- add positional arguments
 
 parser = argparse.ArgumentParser(description="Log changes of configuration fles ")
-parser.add_argument("deployedFile",help='complete path of a deployed file')
-parser.add_argument("archivedFile",help='complete path of a archived file')
+parser.add_argument("deployedFile", help='complete path of a deployed file')
+parser.add_argument("archivedFile", help='complete path of a archived file')
 args = parser.parse_args()
 
 
-print ("First Argument",args.deployedFile)
-print ("Second Argument",args.archivedFile)
-
-
+print("First Argument", args.deployedFile)
+print("Second Argument", args.archivedFile)
 
 
 if not os.path.isfile(args.deployedFile):
     print("stop compare and add logs")
-    msg="%s Deplyed file not exists \n"%(str(datetime.now()))
+    msg = "%s Deplyed file not exists \n"%(str(datetime.now()))
     locallog(msg)
 else:
     print("deployed file exists")
